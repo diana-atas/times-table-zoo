8102bc35377ab6379abc9a8823baf62d1bce932c
Diana Atas <diana.atas@trademe.co.nz>
2023-07-13 16:17:35 +1200

    Initial Commit

---
+++ TimesTableZoo/TimesTableZooApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimesTableZooApp.swift
+//  TimesTableZoo
+//
+//  Created by Diana Atas on 13/07/23.
+//
+
+import SwiftUI
+
+@main
+struct TimesTableZooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
