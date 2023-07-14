a5afccb4ab0352763c610aca8cf93253312d4d75
Diana Atas <diana.atas@trademe.co.nz>
2023-07-15 10:48:45 +1200

    apply basic game settings

--- TimesTableZoo/ContentView.swift
+++ TimesTableZoo/ContentView.swift
@@ -8,12 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var multiplier = Int.random(in: 1...12)
+    private var multiplicand = Int.random(in: 1...12)
+    private var product = 1
+    private var numberOfQuestions = [5, 10, 20]
+    @State private var selectedNumberOfQuestions = 5
+    @State private var isEditing = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Stepper("Pick a multiplier: \(multiplier)", value: $multiplier, in: 1...12)
+            Section {
+                Picker("Pick number of questions", selection: $selectedNumberOfQuestions) {
+                    ForEach(numberOfQuestions, id: \.self) {
+                        Text("\($0)")
+                    }
+                }
+                .pickerStyle(.segmented)
+            } header: {
+                Text("Pick number of questions")
+            }
+            List {
+                ForEach(0..<Int(selectedNumberOfQuestions), id: \.self) {_ in
+                    Text("What is \(multiplier) x \(multiplicand)?")
+                }
+            }
         }
         .padding()
     }
