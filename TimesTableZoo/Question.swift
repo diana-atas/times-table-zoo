17320fd4790999fa012f28418a5247e22cd01450
Diana Atas <diana.atas@trademe.co.nz>
2023-07-26 18:37:47 +1200

    move question to separate file

---
+++ TimesTableZoo/Question.swift
@@ -0,0 +1,13 @@
+//
+//  Question.swift
+//  TimesTableZoo
+//
+//  Created by Diana Atas on 26/07/23.
+//
+
+import Foundation
+
+struct Question {
+    let question: String
+    let correctAnswer: Int
+}
--- TimesTableZoo/QuestionModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  QuestionModel.swift
-//  TimesTableZoo
-//
-//  Created by Diana Atas on 15/07/23.
-//
-
-import Foundation
